72cf41ecf706638758889fddab4abd970d7d8dac
Amit K Sehgal <sehgals@outlook.com>
2019-07-01 15:15:04 +0530

    Rename orderMetaDataManager to orderManager

--- orderManager.js
+++ orderManager.js
@@ -5,17 +5,6 @@ const AWS = require('aws-sdk');
 //const AWS = awsXRay.captureAWS(require('aws-sdk'));
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
-/*
- order : {
-  orderId: String,
-  name: String,
-  address: String,
-  pizzas: Array of Strings,
-  delivery_status: READY_FOR_DELIVERY / DELIVERED
-  timestamp: timestamp
-}
-*/
-
 module.exports.saveCompletedOrder = order => {
 	console.log('saveCompletedOrder function is called !!');
 
